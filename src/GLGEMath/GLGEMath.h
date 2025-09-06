9e49dd4c22792921596c0424ad02d910bf8c2280
DM8AT <DM8AT@users.noreply.github.com>
2025-09-06 12:50:46 +0200

    Initial commit

---
+++ src/GLGEMath/GLGEMath.h
@@ -0,0 +1,18 @@
+/**
+ * @file GLGEMath.h
+ * @author DM8AT
+ * @brief include all math stuff from the GLGE math library
+ * @version 0.1
+ * @date 2025-09-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+//header guard
+#ifndef _GLGE_MATH_
+#define _GLGE_MATH_
+
+
+
+#endif
---
+++ src/GLGEMath/Vector/floats/GLGE_vec2.h
@@ -0,0 +1,14 @@
+/**
+ * @file GLGE_vec2.h
+ * @author DM8AT
+ * @brief a simple 2D vector of floats for GLGE
+ * @version 0.1
+ * @date 2025-09-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+//header guard
+#ifndef _GLGE_FLOAT_VEC2_
+#define _GLGE_FLOAT_VEC2_
